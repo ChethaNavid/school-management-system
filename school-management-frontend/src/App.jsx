3c30d8aaff0b9c8f13cea6cb2ccc20f99d2e13d2
ChethaNavid <164472640+ChethaNavid@users.noreply.github.com>
2025-07-10 18:45:24 +0700

    update packages and App

--- school-management-frontend/src/App.jsx
+++ school-management-frontend/src/App.jsx
@@ -6,13 +6,17 @@ import Courses from "./pages/Courses";
 import Teachers from "./pages/Teachers";
 import Students from "./pages/Students";
 import Navbar from "./components/Navbar";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/dashboard" element={
+        <ProtectedRoute>
+          <Dashboard />
+        </ProtectedRoute>} />
       <Route path="/courses" element={<Courses />} />
       <Route path="/teachers" element={<Teachers />} />
       <Route path="/students" element={<Students />} />
